7f667be6124a969a7ae47632ac341de0c943cc95
Blacktime <lordigna@gmail.com>
2018-06-12 11:32:50 +0200

    Ajout des logs

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
+var fs = require('fs');
 
 app.use(express.static("web"));
 
 app.get("/", function(req, res){
     res.header(200);
     res.sendFile(__dirname+"/web/adress_resolver.html");
+    let ip = req.connection.remoteAddress.substr(7);
+    fs.appendFile('log.txt', `${ip} :  Connexion`);
 });
 
 http.listen(8080);
