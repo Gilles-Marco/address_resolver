f911651420c0be9403cbb954fb2a3971bd062ca8
Blacktime <lordigna@gmail.com>
2018-06-12 11:48:04 +0200

    ajout de l'heure

--- server.js
+++ server.js
@@ -9,7 +9,32 @@ app.get("/", function(req, res){
     res.header(200);
     res.sendFile(__dirname+"/web/adress_resolver.html");
     var ip = req.connection.remoteAddress.substr(7);
-    fs.appendFile('log.txt', `${ip} :  Connexion\n`);
+    fs.appendFile('log.txt', `${getDateTime()} ${ip} :  Connexion\n`);
 });
 
 http.listen(8080);
+
+function getDateTime() {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+
+}
